7e202fb7076f524cab9de9dfed4a175caa0751db
Qalbun Salim Buanaputra <qalbun.salim@ui.ac.id>
2023-06-06 02:00:44 +0700

    reconfigure production settings

--- Calma/settings/production/base.py
+++ Calma/settings/production/base.py
@@ -1,43 +1,16 @@
-from Calma.settings.components.base import REST_FRAMEWORK,BASE_DIR
+from Calma.settings.components.base import REST_FRAMEWORK,INSTALLED_APPS
 import environ
 import os
-import google.auth
 import io
 from urllib.parse import urlparse
-from google.cloud import secretmanager
 
 env = environ.Env()
-env_file = os.path.join(BASE_DIR, ".env")
-DEBUG = False
-
-# Attempt to load the Project ID into the environment, safely failing on error.
-try:
-    _, os.environ["GOOGLE_CLOUD_PROJECT"] = google.auth.default()
-except google.auth.exceptions.DefaultCredentialsError:
-    pass
-
-if os.path.isfile(env_file):
-    # Use a local secret file, if provided
-
-    env.read_env(env_file)
-elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
-    # Pull secrets from Secret Manager
-    project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
-
-    client = secretmanager.SecretManagerServiceClient()
-    settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
-    name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
-    payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
-
-    env.read_env(io.StringIO(payload))
-else:
-    raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
-
+env.read_env(io.StringIO(os.environ.get("AUTH_SECRET", None)))
 
 SECRET_KEY = env("SECRET_KEY")
+DEBUG = env("DEBUG", default=False)
 
-
-# CSRF CONFIGURATIONS
+# If defined, add service URL to Django security settings
 CLOUDRUN_SERVICE_URL = env("CLOUDRUN_SERVICE_URL", default=None)
 if CLOUDRUN_SERVICE_URL:
     ALLOWED_HOSTS = [urlparse(CLOUDRUN_SERVICE_URL).netloc]
--- Calma/settings/production/databases.py
+++ Calma/settings/production/databases.py
@@ -1,8 +1,15 @@
 import environ
 import os
+import io
 
-env = environ.Env()
-env.read_env()
+from Calma.settings.production.base import env
+env.read_env(io.StringIO(os.environ.get("AUTH_SECRET", None)))
+
 DATABASES = {
-  'default': env.db('DATABASE_URL')
+  'default': env.db()
 }
+
+# Change database settings if using the Cloud SQL Auth Proxy
+if os.getenv("USE_CLOUD_SQL_AUTH_PROXY", None):
+    DATABASES["default"]["HOST"] = "127.0.0.1"
+    DATABASES["default"]["PORT"] = 5432
