97cca33db76e6830b7d4b092049d32fda709d83c
Qalbun Salim Buanaputra <qalbun.salim@ui.ac.id>
2023-06-06 12:17:12 +0700

    add cors and whitenoise

--- Calma/settings/components/base.py
+++ Calma/settings/components/base.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     'dj_rest_auth',
@@ -44,6 +45,8 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -85,9 +88,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
@@ -134,3 +134,4 @@ REST_AUTH = {
 }
 AUTH_USER_MODEL = "calma_account.User"
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
